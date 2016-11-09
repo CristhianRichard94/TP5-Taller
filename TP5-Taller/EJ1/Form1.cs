0ac4da1d696cd261afd84ae3a41a5072ef4c2b49
Cristhian Richard <criiss13.-@hotmail.com>
2016-11-09 18:23:11 -0300

    EJ1 hecho

--- TP5-Taller/EJ1/Form1.cs
+++ TP5-Taller/EJ1/Form1.cs
@@ -16,5 +16,35 @@ namespace EJ1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se ha clickeado el boton 1","Cuadro 1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se ha clickeado el boton 2", "Cuadro 2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se ha clickeado el boton 3", "Cuadro 3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se ha clickeado el boton 4", "Cuadro 4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se ha clickeado el boton 5", "Cuadro 5");
+        }
     }
 }
