67611cfd14d33ab75330b7097b02f779fac58c60
Cristhian Richard <criiss13.-@hotmail.com>
2016-11-09 20:25:19 -0300

    empezando ej5

---
+++ TP5-Taller/EJ5/FachadaFigura.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1
+{
+    class FachadaFigura
+    {
+        /// <summary>
+        /// Se encarga de recibir los valores de program y llama al constructor de Triangulo, luego accede a la propiedad Perimetro
+        /// </summary>
+        /// <param name="px1">Coordenada del punto1 en X</param>
+        /// <param name="py1">Coordenada del punto1 en Y</param>
+        /// <param name="px2">Coordenada del punto2 en X</param>
+        /// <param name="py2">Coordenada del punto2 en Y</param>
+        /// <param name="px3">Coordenada del punto3 en X</param>
+        /// <param name="py3">Coordenada del punto3 en Y</param>
+        /// <returns></returns>
+        public double CalcularPerimetroTriangulo(double px1,double py1, double px2,double  py2,double px3,double  py3)
+        {
+            Triangulo t = new Triangulo(new Punto(px1,py1), new Punto(px2,py2),new Punto(px3,py3));
+            return t.Perimetro;
+        }
+        /// <summary>
+        /// Se encarga de recibir los valores de program y llama al constructor de Triangulo, luego accede a la propiedad Area
+        /// </summary>
+        /// <param name="px1">Coordenada del punto1 en X</param>
+        /// <param name="py1">Coordenada del punto1 en Y</param>
+        /// <param name="px2">Coordenada del punto2 en X</param>
+        /// <param name="py2">Coordenada del punto2 en Y</param>
+        /// <param name="px3">Coordenada del punto3 en X</param>
+        /// <param name="py3">Coordenada del punto3 en Y</param>
+        /// <returns></returns>
+        public double CalcularAreaTriangulo(double px1, double py1, double px2, double py2, double px3, double py3)
+        {
+            Triangulo t = new Triangulo(new Punto(px1, py1), new Punto(px2, py2), new Punto(px3, py3));
+            return t.Area;
+        }
+        /// <summary>
+        /// Se encarga de recibir los valores de program y llama al constructor de Circulo, luego accede a la propiedad Perimetro de este
+        /// </summary>
+        /// <param name="px">Coordenada del Centro en X</param>
+        /// <param name="py">Coordenada del Centro en y</param>
+        /// <param name="pr">Valor real del Radio del Circulo</param>
+        /// <returns></returns>
+        public double CalcularPerimetroCirculo(double px, double py, double pr)
+        {
+            Circulo c = new Circulo(px, py, pr);
+            return c.Perimetro;
+        }
+        /// <summary>
+        /// Se encarga de recibir los valores de program y llama al constructor de Triangulo, luego accede a la propiedad Area
+        /// </summary>
+        /// <param name="px">Coordenada del Centro en X</param>
+        /// <param name="py">Coordenada del Centro en y</param>
+        /// <param name="pr">Valor real del Radio del Circulo</param>
+        /// <returns></returns>
+        public double CalcularAreaCirculo(double px, double py, double pr)
+        {
+            Circulo c = new Circulo(px, py, pr);
+            return c.Area;
+        }
+    }
+}
---
+++ TP5-Taller/EJ5/Punto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1
+{
+    class Punto
+    {
+        private double iX;
+        /// <summary>
+        /// Propiedad del atributo X de la clase
+        /// </summary>
+        public double X
+        {
+            get { return this.iX; }
+        }
+        private double iY;
+        
+        /// <summary>
+        /// Propiedad del atributo Y de la clase
+        /// </summary>
+        public double Y
+        {
+            get { return this.iY; }
+        }
+        /// <summary>
+        /// Constructor de la clase 
+        /// </summary>
+        /// <param name="pX"> coordenada del punto en X</param>
+        /// <param name="pY">coordenada del punto en Y</param>
+        public Punto(double pX, double pY)
+        {
+            this.iX = pX;
+            this.iY = pY;
+        }
+        /// <summary>
+        /// Calcula la distancia desde un punto a otro como Raiz cuadrada de la suma de los cuadrados de las distancias entre las componentes x e y
+        /// </summary>
+        /// <param name="p">punto en componentes x e y</param>
+        /// <returns>Devuelve un valor real que representa la distancia entre ambos puntos</returns>
+        public double calcularDistanciaDesde(Punto p)    
+        {                                               
+            return Math.Sqrt(Math.Pow((this.iX - p.iX), 2) + Math.Pow((this.iY - p.iY), 2));
+        }
+    }
+}
